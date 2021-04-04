fe368ea92c6f0a6d5155a03ce44bdfd4d62f4550
Kausthub K (AuroraU20) <kausthub.k@live.com>
2021-04-05 09:17:32 +1000

    Visual Studio C++ cross compilable app

---
+++ skeleton_code/cpp_cross_platform_skeleton/cpp_cross_platform_skeleton.cpp
@@ -0,0 +1,12 @@
+﻿// cpp_cross_platform_skeleton.cpp : Defines the entry point for the application.
+//
+
+#include "cpp_cross_platform_skeleton.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
